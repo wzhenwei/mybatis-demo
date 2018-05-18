1108c3c8a4f7e9e97952f464dc4ba2b7dc19f708
wangzhenwei <wangzhenwei@wzw.com>
2018-05-18 11:28:47 +0800

    myabatis初次尝试

---
+++ src/main/java/tk/mybatis/simple/mapper/CountryMapperTest.java
@@ -0,0 +1,50 @@
+package tk.mybatis.simple.mapper;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import tk.mybatis.simple.model.Country;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+/**
+ * @author wangzhenwei
+ * @date 2018/5/16 17:35
+ */
+public class CountryMapperTest {
+
+    private static SqlSessionFactory sqlSessionFactory;
+    @BeforeClass
+    public static void init(){
+        try {
+            Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testSelectAll(){
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            List<Country> countryList = sqlSession.selectList("selectAll");
+            printCountryList(countryList);
+        }finally {
+            // 不要忘记关闭sqlSession
+            sqlSession.close();
+        }
+    }
+
+    private void printCountryList(List<Country> countryList){
+        for (Country country : countryList){
+            System.out.printf("%-4d%4s%4s\n",country.getId(),country.getCountryname(),country.getCountrycode());
+        }
+    }
+}
